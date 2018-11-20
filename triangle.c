1b6fe5df6cb07d4d1971042a76b8b9eda0be70a6
Tarun65 <41103813+Tarun65@users.noreply.github.com>
2018-11-20 21:36:59 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int mian()
+{
+	int a,b,c;
+	float area,p;
+	printf("Enter the side of triangle:A\nB\nc\n");
+	scanf("%d%d%d",&a,&b,&c);
+	p=(a+b+c)/2;
+    area=sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("the area of triangle:%f",area);
+    return 0;
+	
+}
